1ff1c34c6532d10e75c8181ef52f38fff35ed26f
MillyCat <emily.savczuk@escola.pr.gov.br>
2025-02-19 11:40:26 -0300

    IUUUUUUPIIIII

--- aula1.py
+++ aula1.py
@@ -2,6 +2,11 @@ import random
 sorteio = random.randint(1, 10)
 print("Jogo da adivinhação")
 print("Adivinhe o número que estou pensando de 1 a 10")
-chute = input("Digite o seu chute:")
+chute = int(input("Digite o seu chute:"))
 print(sorteio)
 print(chute)
+if (chute == sorteio):
+    print("EBAAAAAAAAAA VC ACERTOU, O numero era:", sorteio)
+if (chute != sorteio):
+    print("Voce errou ;n; O numero era:", sorteio)    
+ 
