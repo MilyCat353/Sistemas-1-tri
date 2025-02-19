ff21a8937ab9c47fc65251d5854f5aca33ca789a
MillyCat <emily.savczuk@escola.pr.gov.br>
2025-02-19 11:23:10 -0300

    arquivo salvandop

--- aula1.py
+++ aula1.py
@@ -1,5 +1,7 @@
 import random
 sorteio = random.randint(1, 10)
+print("Jogo da adivinhação")
+print("Adivinhe o número que estou pensando de 1 a 10")
 chute = input("Digite o seu chute:")
 print(sorteio)
 print(chute)
