150e5cb4a5418dbe1a0305542e2f616dc0f9a455
MilyCat353 <146204164+MilyCat353@users.noreply.github.com>
2025-02-24 11:30:34 -0300

    entrada

---
+++ Entradadeestudante/entrada.js
@@ -0,0 +1,10 @@
+let estudantes = true;
+let estudiosos = false;
+if(estudantes){
+    console.log("tem direito a meia entrada!");
+} else {
+    console.log("Pague inteira");
+}
+// Operadores javascript
+console.log(true == 1);
+console.log(false == 0);
