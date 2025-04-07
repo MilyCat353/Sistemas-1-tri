6ab5a873d2839d4129e014308aacec5e3beb9a51
MilyCat353 <146204164+MilyCat353@users.noreply.github.com>
2025-04-07 11:47:15 -0300

    Update forca.py

--- adivinhacao.py/Forca/forca.py
+++ adivinhacao.py/Forca/forca.py
@@ -1,19 +1,32 @@
 palavra = "escola"
 limite_tentativas = len(palavra) + 5
+acertou = False
+enforcou = False
+
 letras_acertadas = []
 for letra in palavra:
     letras_acertadas.append("-")
+
 contador = 1
-while(contador <= limite_tentativas):
+while(not acertou and not enforcou):
     print(letras_acertadas)
     print("Tentativas: ", contador, "/", limite_tentativas)
     chute = input("Digite a letra: ").lower()
+
     indice = 0
-
-
     for letra in palavra:
         if chute == letra:
             letras_acertadas[indice] = chute
-        indice = indice + 1 
+        indice = indice + 1
+
+    if contador == limite_tentativas:
+        enforcou = True
+        print("Você perdeu!")
+        print(palavra)
+
+    if letras_acertadas.count("-") == 0:
+        acertou = True
+        print("Parabêns!! Você acertou!")
+        print(letras_acertadas)
 
     contador = contador + 1
