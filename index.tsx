8437048a69c3c68d872e0b1905b7cc441815acab
MilyCat353 <146204164+MilyCat353@users.noreply.github.com>
2025-04-09 12:15:07 -0300

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,29 +1,84 @@
 import { Text, View, StyleSheet } from "react-native";
-import {Image} from "expo-image"
+import { Image } from "expo-image"
+import { Ionicons } from "@expo/vector-icons"
 
-const foto = require("../assets/images/react-logo.png");
+// npm install expo-image (usar este código no terminal)
+
+const foto = require("../assets/images/download.jpeg")
+
 export default function Index() {
   return (
-    <View style={estilos.container}>
-      <Image source={foto} style={estilos.foto}/>
-      <Text style= {estilos.texto}> Edit app/index.tsk to edit this screen</Text>
+    <View style={styles.container} >
+      <View style = {styles.containerImg}>
+        <Image source = {foto} style={styles.estiloFoto}></Image>
+      </View>
+      <View style = {styles.containerConteudo}>
+        <View style = {styles.containerNome}>
+          <Text style={styles.nome}>Emily Savczuk</Text>
+        </View>
+        <Text style = {styles.linha}>
+          _______________________________________
+        </Text>
+        <View style = {styles.containerDados}>
+          <Ionicons name="person" size={24} color="pink" />
+          <Text style = {styles.textoDados}>16 anos</Text>
+        </View>
+        <View style = {styles.containerDados}>
+          <Ionicons name="mail" size={24} color="pink" />
+          <Text style = {styles.textoDados}>emily.savczuk@escola.pr.gov.br</Text>
+        </View>
+        <View style = {styles.containerDados}>
+          <Ionicons name="call" size={24} color="pink" />
+          <Text style = {styles.textoDados}>(42) 99999-6666</Text>
+        </View>
+        <View style = {styles.containerDados}>
+          <Ionicons name="home" size={24} color="pink" />
+          <Text style = {styles.textoDados}>Ponta Grossa / PR</Text>
+        </View>
+      </View>
     </View>
   );
 }
-const estilos = StyleSheet.create({
+
+const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "grey",
+    backgroundColor: "white"
   },
-  foto: {
+  containerImg: {
+    flex: 1,
+    paddingTop: 50,
+  },
+  estiloFoto: {
     width: 300,
     height: 300,
-
   },
-  texto: {
-    color: "White",
-    fontSize: 20
+  containerConteudo: {
+    flex: 1
+  },
+  containerNome:{
+    alignItems:"center",
+  },
+  nome: {
+    fontSize: 40,
+    color: "pink",
+    fontWeight: "bold",
+  },
+  linha:{
+    color: "pink",
+    fontSize: 20,
+    marginBottom: 20
+  },
+  containerDados: {
+    marginBottom: 8,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  textoDados: {
+    marginLeft: 10,
+    color: "pink",
+    fontSize: 24,
   }
 })
