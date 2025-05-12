be5289adf255c23bbb67870837b7465a458ba1b7
MilyCat353 <146204164+MilyCat353@users.noreply.github.com>
2025-05-12 12:18:25 -0300

    Update revisao.py

--- Revisao/revisao.py
+++ Revisao/revisao.py
@@ -4,8 +4,7 @@ print("Bem vindo ao sistema!!")
 altura = 1.65
 musica = "Dandelions"
 SerieFavorita = "Game Off Trones"
-notaSerie = 8.63
-altura = 1.65
+notaSerie = 9.2
 AlturaUsuario = float(input("digite sua altura:"))
 if altura > AlturaUsuario:
     print("Você é mais baixo")
@@ -25,18 +24,17 @@ if musica in musicaUsuario:
 else:
     print("Saia daqui, você não é como eu, seu sem alma.")
 
-nota_favorito = 8.63
 filmes = []
 notas = []
 for i in range(3):
-    filme = input(f"Digite o nome do filme {i+1}: ")
-    nota = float(input(f"Digite a nota do IMDB do filme {i+1}: "))
+    filme = input(f"Digite o nome da serie:{i+1}: ")
+    nota = float(input(f"Digite a nota do IMDB da serie {i+1}: "))
     filmes.append(filme)
     notas.append(nota)
 for i in range(3):
-    if notas[i] > nota_favorito:
-        print(f"O filme '{filmes[i]}' tem nota maior que o meu filme favorito.")
-    elif notas[i] < nota_favorito:
-        print(f"O filme '{filmes[i]}' tem nota menor que o meu filme favorito.")
+    if notas[i] > notaSerie:
+        print(f"A serie'{filmes[i]}' tem nota maior que a minha serie favorita.")
+    elif notas[i] < notaSerie:
+        print(f"A serie '{filmes[i]}' tem nota menor que a minha serie favorita.")
     else:
-        print(f"O filme '{filmes[i]}' tem a mesma nota que o meu filme favorito.")
+        print(f"A serie '{filmes[i]}' tem a mesma nota que a minha serie favorita.")
