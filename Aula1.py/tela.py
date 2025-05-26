6b470cfe68433ff07dac8fff3577ca1cd6fdd7ff
MilyCat353 <146204164+MilyCat353@users.noreply.github.com>
2025-05-26 12:22:22 -0300

    Update tela.py

--- Aula1.py/tela.py
+++ Aula1.py/tela.py
@@ -2,4 +2,9 @@
 from Conta import Conta
 
 conta1 = Conta(123, "12366-66", "Katrina", 3.0, 1234)
+conta2 = Conta(333, "45245-99", "AnaMariaBraga", 50.0, 4231)
 print(conta1.extrato())
+print(conta2.extrato())
+conta1.pix(2, conta2)
+print(conta1.extrato())
+print(conta2.extrato())
